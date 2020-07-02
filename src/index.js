75beb57b8bce085a4bfbe358fbd387ce0c1dd141
anselbrandt <ansel@anselbrandt.com>
2020-07-02 03:18:55 -0400

    Animate time

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles.css";
+
+const deg = 6;
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+const render = () => {
+  const day = new Date();
+  const hh = day.getHours() * 30;
+  const mm = day.getMinutes() * deg;
+  const ss = day.getSeconds() * deg;
+
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+  requestAnimationFrame(render);
+};
+
+requestAnimationFrame(render);
